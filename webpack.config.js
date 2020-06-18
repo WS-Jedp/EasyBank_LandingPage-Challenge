5e62f5951a0f47b1e4a61d9e0781bf084b00c2d3
Jedp <jedp082@gmail.com>
2020-06-18 14:41:52 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/app.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css?$/,
+        exclude: /node_modules/,
+        use: [
+          miniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { 
+              url: false,
+              sourceMap: true,
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[local]___[hash:base64:5]'
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(svg|.png|.gif|.jpg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/imgs/[name].[ext]'
+            }
+          }
+        ]
+
+      },
+      {
+        test: /\.s[ac]ss/,
+        use: [
+          miniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.styl?$/,
+        use: [
+          miniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'stylus-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new miniCssExtractPlugin({
+      filename: 'assets/styles/[name].css',
+      chunkFilename: 'assets/styles/[id].css'
+    })
+  ]
+}
