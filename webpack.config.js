7c3dca9ef3112fa0fdc9d0d139cd3f10ac2c274f
Jedp <jedp082@gmail.com>
2020-06-25 19:36:43 -0500

    Dist and resources fixed

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              publicPath: '/assets/images',
+              publicPath: 'assets/images',
               outputPath: 'assets/images'
             }
           }
